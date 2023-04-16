bd0892ca597dbe7400765f3da0c0dfe4d9ba9b15
abhishekkumar <abhi.kr0550@gmail.com>
2023-04-15 22:18:54 -0400

    Imperial Vote Application

---
+++ components/home-two/feature-section-two.js
@@ -0,0 +1,82 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-img-element */
+
+export default function FeatureSectionTwo() {
+	return (
+		<div className="section bg-gray-800 fugu-section-padding3">
+			<div className="container">
+				<div className="fugu-section-title title-white">
+					<u><h2>Development Team</h2></u>
+				</div>
+				<div className="row">
+					<div className="col-lg-4 col-md-6">
+						<div className="fugu-iconbox-wrap3 wow fadeInUpX" data-wow-delay="0s">
+							<div className="fugu-iconbox-icon3">
+								<img className="iv-contributors" src="/images/all-img/v2/profile1.png" alt="" />
+							</div>
+							<div className="fugu-iconbox-data3">
+								<h4>Developer-1</h4>
+
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-4 col-md-6">
+						<div className="fugu-iconbox-wrap3 wow fadeInUpX" data-wow-delay="0.10s">
+							<div className="fugu-iconbox-icon3">
+								<img className="iv-contributors" src="/images/all-img/v2/profile1.png" alt="" />
+							</div>
+							<div className="fugu-iconbox-data3">
+								<h4>Developer-2</h4>
+
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-4 col-md-6">
+						<div className="fugu-iconbox-wrap3 wow fadeInUpX" data-wow-delay="0.20s">
+							<div className="fugu-iconbox-icon3">
+								<img className="iv-contributors" src="/images/all-img/v2/profile1.png" alt="" />
+							</div>
+							<div className="fugu-iconbox-data3">
+								<h4>Developer-3</h4>
+
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-4 col-md-6">
+						<div className="fugu-iconbox-wrap3 wow fadeInUpX" data-wow-delay="0.30s">
+							<div className="fugu-iconbox-icon3">
+								<img className="iv-contributors" src="/images/all-img/v2/profile1.png" alt="" />
+							</div>
+							<div className="fugu-iconbox-data3">
+								<h4>Developer-4</h4>
+
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-4 col-md-6">
+						<div className="fugu-iconbox-wrap3 wow fadeInUpX" data-wow-delay="0.40s">
+							<div className="fugu-iconbox-icon3">
+								<img className="iv-contributors" src="/images/all-img/v2/profile1.png" alt="" />
+							</div>
+							<div className="fugu-iconbox-data3">
+								<h4>Developer-5</h4>
+
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-4 col-md-6">
+						<div className="fugu-iconbox-wrap3 wow fadeInUpX" data-wow-delay="0.50s">
+							<div className="fugu-iconbox-icon3">
+								<img className="iv-contributors" src="/images/all-img/v2/profile1.png" alt="" />
+							</div>
+							<div className="fugu-iconbox-data3">
+								<h4>Developer-6</h4>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
